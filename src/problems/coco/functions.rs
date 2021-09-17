6bbebd80241585ca3aa3f0ac96b6f6eb903eb5dd
Leopold Luley <git@leopoldluley.de>
2021-09-17 16:11:39 +0900

    Add coco input and output translation.

--- src/problems/coco/functions.rs
+++ src/problems/coco/functions.rs
@@ -5,6 +5,56 @@ pub fn sphere(x: &[f64]) -> f64 {
     x.iter().map(|xi| xi * xi).sum()
 }
 
+pub fn ellipsoid(x: &[f64]) -> f64 {
+    let condition = 1.0e6f64;
+    let n = x.len() as f64;
+
+    x.iter()
+        .enumerate()
+        .map(|(i, xi)| (i as f64, *xi))
+        .map(|(i, xi)| condition.powf(i / (n - 1.0)) * xi * xi)
+        .sum()
+}
+
+/// Rastrigin
+pub fn rastrigin(x: &[f64]) -> f64 {
+    let num = x.len() as f64;
+    let sum1 = x.iter().map(|&xi| (2.0 * PI * xi).cos()).sum::<f64>();
+    let sum2 = x.iter().map(|&xi| xi * xi).sum::<f64>();
+
+    10.0 * (num - sum1) + sum2
+}
+
+/// Büche Rastrigin
+///
+/// This is the same as [rastrigin]
+pub fn bueche_rastrigin(x: &[f64]) -> f64 {
+    rastrigin(x)
+}
+
+pub fn linear_slope(x: &[f64]) -> f64 {
+    let n = x.len() as f64;
+
+    x.iter()
+        .enumerate()
+        .map(|(i, xi)| (i as f64, *xi))
+        .map(|(i, xi)| (10.0f64.powf(i / (n - 1.0)), xi))
+        .map(|(si, xi)| 5.0 * (si.abs() - f64::min(5.0, xi)))
+        .sum::<f64>()
+}
+
+pub fn rosenbrock(x: &[f64]) -> f64 {
+    let sum1 = x
+        .iter()
+        .zip(x.iter().skip(1))
+        .map(|(xi, xj)| xi * xi - xj)
+        .map(|xi| xi * xi)
+        .sum::<f64>();
+    let sum2 = x.iter().map(|xi| xi - 1.0).map(|xi| xi * xi).sum::<f64>();
+
+    100.0 * sum1 + sum2
+}
+
 /// Attractive Sector
 ///
 /// Important: This deviates from coco!
@@ -40,12 +90,4 @@ pub fn attractive_sector(x: &[f64]) -> f64 {
     }
 
     x.iter().map(|&xi| factor(xi) * xi * xi).sum()
-}
-
-pub fn rastrigin(x: &[f64]) -> f64 {
-    let num = x.len() as f64;
-    let sum1 = x.iter().map(|&xi| (2.0 * PI * xi).cos()).sum::<f64>();
-    let sum2 = x.iter().map(|&xi| xi * xi).sum::<f64>();
-
-    10.0 * (num - sum1) + sum2
 }
