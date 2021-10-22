879982353862ed48391ac604297a093f3d250384
Leopold Luley <git@leopoldluley.de>
2021-10-22 12:25:23 +0900

    Cleanup BBOB suite constructor.

--- src/problems/coco/suits/bbob.rs
+++ src/problems/coco/suits/bbob.rs
@@ -5,12 +5,21 @@ use std::ops::RangeInclusive;
 
 mod util_2009;
 
-static YEARS: &[usize] = &[2009, 2010, 2012, 2013, 2015, 2016, 2017, 2018];
+pub enum Years {
+    Y2009 = 2009,
+    Y2010 = 2010,
+    Y2012 = 2012,
+    Y2013 = 2013,
+    Y2015 = 2015,
+    Y2016 = 2016,
+    Y2017 = 2017,
+    Y2018 = 2018,
+}
 
-pub fn new() -> Suite {
+pub fn new(year: Years) -> Suite {
     Suite::new(
         flatten_ranges(&[1..=24]),
-        vec![1],
+        instances_by_year(year),
         vec![2, 3, 5, 10, 20, 40],
         generator,
     )
@@ -28,20 +37,19 @@ fn flatten_ranges(ranges: &[RangeInclusive<usize>]) -> Vec<usize> {
     list
 }
 
-fn instances_by_year(year: usize) -> Option<Vec<usize>> {
-    let ranges: Option<&[_]> = match year {
-        2009 => Some(&[1..=5, 1..=5, 1..=5]),
-        2010 => Some(&[1..=15]),
-        2012 => Some(&[1..=5, 21..=30]),
-        2013 => Some(&[1..=5, 31..=40]),
-        2015 => Some(&[1..=5, 41..=50]),
-        2016 => Some(&[1..=5, 51..=60]),
-        2017 => Some(&[1..=5, 61..=70]),
-        2018 => Some(&[1..=5, 71..=80]),
-        _ => None,
+fn instances_by_year(year: Years) -> Vec<usize> {
+    let ranges: &[_] = match year {
+        Years::Y2009 => &[1..=5, 1..=5, 1..=5],
+        Years::Y2010 => &[1..=15],
+        Years::Y2012 => &[1..=5, 21..=30],
+        Years::Y2013 => &[1..=5, 31..=40],
+        Years::Y2015 => &[1..=5, 41..=50],
+        Years::Y2016 => &[1..=5, 51..=60],
+        Years::Y2017 => &[1..=5, 61..=70],
+        Years::Y2018 => &[1..=5, 71..=80],
     };
 
-    ranges.map(flatten_ranges)
+    flatten_ranges(ranges)
 }
 
 fn generator(function: usize, instance: usize, dimension: usize) -> Instance {
@@ -52,13 +60,15 @@ fn generator(function: usize, instance: usize, dimension: usize) -> Instance {
     let rseed_17 = 17 + 10000 * instance;
 
     let problem = match function {
+        0 => panic!("Suite functions start at 1, 0 was requested"),
         1 => functions::sphere(function, instance, dimension, rseed),
         2 => functions::ellipsoid(function, instance, dimension, rseed),
         3 => functions::rastrigin(function, instance, dimension, rseed),
         4 => functions::bueche_rastrigin(function, instance, dimension, rseed_3),
         5 => functions::linear_slope(function, instance, dimension, rseed),
+        6..=24 => todo!("These are not implemented yet"),
         _ => panic!(
-            "Toy suite only contains 6 functions ({} was requested)",
+            "BBOB suite only contains 24 functions ({} was requested)",
             function
         ),
     };
