4b13655a7178446619ddc26e9d6e902770fb2ec3
Helena Stegherr <helena.stegherr@informatik.uni-augsburg.de>
2021-12-16 15:59:11 +0100

    Add statistics and plotting for population diversity

--- evaluation/eval/calc_stats.py
+++ evaluation/eval/calc_stats.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
 
-__all__ = ["basic_statistics"]
+__all__ = ["basic_statistics", "diversity_statistics"]
 
 
+# calculate basic statistics for best found solution depending on column (usually either iterations or evaluations)
 def basic_statistics(df, column):
     stats_df = df.groupby(column).agg(
         mean_opt=pd.NamedAgg(column="best_fx", aggfunc=np.mean),
@@ -15,3 +16,15 @@ def basic_statistics(df, column):
     pd.DataFrame(stats_df)
     return stats_df
 
+
+# calculate basic statistics for population diversity depending on column (usually either iterations or evaluations)
+def diversity_statistics(df, column):
+    stats_df = df.groupby(column).agg(
+        mean_div=pd.NamedAgg(column="diversity", aggfunc=np.mean),
+        std_div=pd.NamedAgg(column="diversity", aggfunc=np.std),
+        min_div=pd.NamedAgg(column="diversity", aggfunc="min"),
+        max_div=pd.NamedAgg(column="diversity", aggfunc="max"),
+        median_div=pd.NamedAgg(column="diversity", aggfunc=np.median),
+    )
+    pd.DataFrame(stats_df)
+    return stats_df
