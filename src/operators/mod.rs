5274f0c6c562bb76ab3cebff9e22d1de8d32a91f
Saethox <joni.htruw@gmail.com>
2022-05-11 13:07:55 +0200

    Add Noop component

--- src/operators/mod.rs
+++ src/operators/mod.rs
@@ -2,6 +2,10 @@
 
 #![allow(clippy::new_ret_no_self)]
 
+use crate::framework::components::Component;
+use crate::framework::State;
+use crate::problems::Problem;
+
 pub mod archive;
 pub mod custom_state;
 pub mod generation;
@@ -14,3 +18,20 @@ pub mod termination;
 
 #[cfg(test)]
 pub mod testing;
+
+/// Doesn't do anything.
+#[derive(Serialize)]
+pub struct Noop;
+impl Noop {
+    pub fn new<P>() -> Box<dyn Component<P>>
+    where
+        P: Problem,
+    {
+        Box::new(Self)
+    }
+}
+impl<P: Problem> Component<P> for Noop {
+    fn execute(&self, _problem: &P, _state: &mut State) {
+        // Noop
+    }
+}
