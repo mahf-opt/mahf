10069494c29cc7195a882f2f4546f6788302c0e5
Saethox <joni.htruw@gmail.com>
2022-05-23 21:36:25 +0200

    Add more specialized component traits

--- src/framework/specializations.rs
+++ src/framework/specializations.rs
@@ -64,6 +64,9 @@ where
 
 /// Specialized component trait to generate a new population from the current one.
 ///
+/// This trait is especially useful for components which modify solutions independently.
+/// For mixing multiple solutions, see [Recombination].
+///
 /// # Implementing [Component]
 ///
 /// Types implementing this trait can implement [Component] by wrapping the type in a [Generator].
@@ -83,6 +86,111 @@ where
         let mut population = state.population_stack_mut().pop();
         self.0.generate_population(&mut population, problem, state);
         state.population_stack_mut().push(population);
+    }
+}
+
+/// Specialized component trait to mutate individual positions or solutions as a whole.
+///
+/// This is a sub-trait to [Generation].
+///
+/// # Implementing [Component]
+///
+/// Types implementing this trait can implement [Component] by wrapping the type either
+/// in a [PositionMutator] or [SolutionMutator], depending on the use case.
+/// The mutator must then be wrapped in a [Generator] to implement [Component].
+pub trait Mutation<P, F> {
+    fn mutation_func<'p>(
+        &self,
+        problem: &'p P,
+        state: &'p mut State,
+    ) -> Box<dyn FnMut(&mut F) + 'p>;
+}
+
+/// Implements [Generation] by calling [Mutation::mutation_func] on individual solution positions.
+#[derive(serde::Serialize)]
+pub struct PositionMutator<T>(pub T);
+
+impl<T, P, F> Generation<P> for PositionMutator<T>
+where
+    P: Problem,
+    for<'a> &'a mut <P as Problem>::Encoding: IntoIterator<Item = &'a mut F>,
+    T: AnyComponent + Mutation<P, F> + Serialize,
+{
+    fn generate_population(
+        &self,
+        population: &mut Vec<Individual>,
+        problem: &P,
+        state: &mut State,
+    ) {
+        let mut mutation = self.0.mutation_func(problem, state);
+        for solution in population.iter_mut() {
+            let solution = solution.solution_mut::<P::Encoding>();
+            for x in solution.into_iter() {
+                mutation(x);
+            }
+        }
+    }
+}
+
+/// Implements [Generation] by calling [Mutation::mutation_func] on individual solutions.
+#[derive(serde::Serialize)]
+pub struct SolutionMutator<T>(pub T);
+
+impl<T, P, D> Generation<P> for SolutionMutator<T>
+where
+    D: Clone + 'static,
+    P: Problem<Encoding = Vec<D>>,
+    T: AnyComponent + Mutation<P, Vec<D>> + Serialize,
+{
+    fn generate_population(
+        &self,
+        population: &mut Vec<Individual>,
+        problem: &P,
+        state: &mut State,
+    ) {
+        let mut mutation = self.0.mutation_func(problem, state);
+        for solution in population.iter_mut() {
+            let solution = solution.solution_mut::<P::Encoding>();
+            mutation(solution);
+        }
+    }
+}
+
+/// Specialized component trait to generate a new population from the current one.
+///
+/// This trait is especially useful for components which mix multiple solutions.
+/// For modifying solutions independently, see [Generation].
+///
+/// # Implementing [Component]
+///
+/// Types implementing this trait can implement [Component] by wrapping the type in a [Recombinator].
+pub trait Recombination<P, F> {
+    fn recombine_solutions(&self, parents: Vec<F>, offspring: &mut Vec<F>, state: &mut State);
+}
+
+#[derive(serde::Serialize)]
+pub struct Recombinator<T>(pub T);
+
+impl<T, P, D> Component<P> for Recombinator<T>
+where
+    P: Problem<Encoding = Vec<D>>,
+    T: AnyComponent + Recombination<P, Vec<D>> + Serialize,
+    D: Clone + PartialEq + 'static,
+{
+    fn execute(&self, _problem: &P, state: &mut State) {
+        let population = state.population_stack_mut().pop();
+        let population = population
+            .into_iter()
+            .map(|i| i.into_solution::<P::Encoding>())
+            .collect();
+        let mut offspring = Vec::new();
+        self.0
+            .recombine_solutions(population, &mut offspring, state);
+        let offspring = offspring
+            .into_iter()
+            .map(Individual::new_unevaluated)
+            .collect();
+        state.population_stack_mut().push(offspring);
     }
 }
 
