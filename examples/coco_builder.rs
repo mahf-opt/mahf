9a6a29920557c75af2c59aa934c5e99e20f985c7
Saethox <joni.htruw@gmail.com>
2022-05-30 15:29:57 +0200

    Change builder to lambda version

--- examples/coco_builder.rs
+++ examples/coco_builder.rs
@@ -4,16 +4,16 @@ use mahf::problems::coco_bound::suits;
 
 fn main() -> anyhow::Result<()> {
     let output = "data/coco/iwo";
-    #[rustfmt::skip]
     let config = ConfigurationBuilder::new()
         .do_(initialization::RandomSpread::new(5))
         .do_(components::SimpleEvaluator::new())
-        .while_(termination::FixedIterations::new(500))
-            .do_(selection::DeterministicFitnessProportional::new(0, 5))
-            .do_(generation::IWOAdaptiveDeviationDelta::new(0.5, 0.001, 3))
-            .do_(components::SimpleEvaluator::new())
-            .do_(replacement::MuPlusLambda::new(20))
-        .while_end()
+        .while_(termination::FixedIterations::new(500), |builder| {
+            builder
+                .do_(selection::DeterministicFitnessProportional::new(0, 5))
+                .do_(generation::IWOAdaptiveDeviationDelta::new(0.5, 0.001, 3))
+                .do_(components::SimpleEvaluator::new())
+                .do_(replacement::MuPlusLambda::new(20))
+        })
         .build();
 
     let suite = suits::bbob();
--- src/framework/builder.rs
+++ src/framework/builder.rs
@@ -1,19 +1,9 @@
-use crate::framework::components::{Block, Branch, Component, Condition, Loop};
+use crate::framework::components::{Block, Branch, Component, Condition, Loop, Scope};
 use crate::framework::Configuration;
 use crate::problems::Problem;
 
-pub trait ComponentIntegration<P> {
-    fn integrate(&mut self, component: Box<dyn Component<P>>);
-}
-
 pub struct ConfigurationBuilder<P> {
     components: Vec<Box<dyn Component<P>>>,
-}
-
-impl<P> ComponentIntegration<P> for ConfigurationBuilder<P> {
-    fn integrate(&mut self, component: Box<dyn Component<P>>) {
-        self.components.push(component);
-    }
 }
 
 impl<P: Problem + 'static> Default for ConfigurationBuilder<P> {
@@ -34,131 +24,48 @@ impl<P: Problem + 'static> ConfigurationBuilder<P> {
         self
     }
 
-    pub fn while_(self, condition: Box<dyn Condition<P>>) -> LoopBuilder<P, Self> {
-        LoopBuilder::new(condition, self)
+    pub fn while_(
+        self,
+        condition: Box<dyn Condition<P>>,
+        body: impl FnOnce(ConfigurationBuilder<P>) -> ConfigurationBuilder<P>,
+    ) -> Self {
+        let components = body(ConfigurationBuilder::new()).components;
+        self.do_(Loop::new(condition, components))
     }
 
-    pub fn if_(self, condition: Box<dyn Condition<P>>) -> BranchBuilder<P, Self> {
-        BranchBuilder::new(condition, self)
+    pub fn if_(
+        self,
+        condition: Box<dyn Condition<P>>,
+        body: impl FnOnce(ConfigurationBuilder<P>) -> ConfigurationBuilder<P>,
+    ) -> Self {
+        let components = body(ConfigurationBuilder::new()).components;
+        self.do_(Branch::new(condition, components))
+    }
+
+    pub fn if_else_(
+        self,
+        condition: Box<dyn Condition<P>>,
+        if_body: impl FnOnce(ConfigurationBuilder<P>) -> ConfigurationBuilder<P>,
+        else_body: impl FnOnce(ConfigurationBuilder<P>) -> ConfigurationBuilder<P>,
+    ) -> Self {
+        let if_components = if_body(ConfigurationBuilder::new()).components;
+        let else_components = else_body(ConfigurationBuilder::new()).components;
+        self.do_(Branch::new_with_else(
+            condition,
+            if_components,
+            else_components,
+        ))
+    }
+
+    pub fn scope_(
+        self,
+        body: impl FnOnce(ConfigurationBuilder<P>) -> ConfigurationBuilder<P>,
+    ) -> Self {
+        let components = body(ConfigurationBuilder::new()).components;
+        self.do_(Scope::new(components))
     }
 
     pub fn build(self) -> Configuration<P> {
         Block::new(self.components)
-    }
-}
-
-pub struct LoopBuilder<P, B> {
-    condition: Box<dyn Condition<P>>,
-    body: Vec<Box<dyn Component<P>>>,
-    parent: B,
-}
-
-impl<P, B> ComponentIntegration<P> for LoopBuilder<P, B> {
-    fn integrate(&mut self, component: Box<dyn Component<P>>) {
-        self.body.push(component);
-    }
-}
-
-impl<P: Problem + 'static, B: ComponentIntegration<P>> LoopBuilder<P, B> {
-    pub fn new(condition: Box<dyn Condition<P>>, parent: B) -> Self {
-        Self {
-            condition,
-            body: Vec::new(),
-            parent,
-        }
-    }
-
-    pub fn do_(mut self, component: Box<dyn Component<P>>) -> Self {
-        self.body.push(component);
-        self
-    }
-
-    pub fn while_(self, condition: Box<dyn Condition<P>>) -> LoopBuilder<P, Self> {
-        LoopBuilder::new(condition, self)
-    }
-
-    pub fn if_(self, condition: Box<dyn Condition<P>>) -> BranchBuilder<P, Self> {
-        BranchBuilder::new(condition, self)
-    }
-
-    pub fn while_end(self) -> B {
-        let Self {
-            condition,
-            body,
-            mut parent,
-        } = self;
-        let component = Loop::new(condition, body);
-        parent.integrate(component);
-        parent
-    }
-}
-
-pub struct BranchBuilder<P, B> {
-    condition: Box<dyn Condition<P>>,
-    if_body: Vec<Box<dyn Component<P>>>,
-    else_body: Option<Vec<Box<dyn Component<P>>>>,
-    if_phase: bool,
-    parent: B,
-}
-
-impl<P, B> ComponentIntegration<P> for BranchBuilder<P, B> {
-    fn integrate(&mut self, component: Box<dyn Component<P>>) {
-        if self.if_phase {
-            self.if_body.push(component);
-        } else {
-            self.else_body.as_mut().unwrap().push(component);
-        }
-    }
-}
-
-impl<P: Problem + 'static, B: ComponentIntegration<P>> BranchBuilder<P, B> {
-    pub fn new(condition: Box<dyn Condition<P>>, parent: B) -> Self {
-        Self {
-            condition,
-            if_body: Vec::new(),
-            if_phase: true,
-            else_body: None,
-            parent,
-        }
-    }
-
-    pub fn do_(mut self, component: Box<dyn Component<P>>) -> Self {
-        if self.if_phase {
-            self.if_body.push(component);
-        } else {
-            self.else_body.as_mut().unwrap().push(component);
-        }
-        self
-    }
-
-    pub fn if_(self, condition: Box<dyn Condition<P>>) -> BranchBuilder<P, Self> {
-        BranchBuilder::new(condition, self)
-    }
-
-    pub fn else_(mut self) -> Self {
-        self.if_phase = false;
-        self
-    }
-
-    pub fn while_(self, condition: Box<dyn Condition<P>>) -> LoopBuilder<P, Self> {
-        LoopBuilder::new(condition, self)
-    }
-
-    pub fn if_end(self) -> B {
-        let Self {
-            condition,
-            if_body,
-            else_body,
-            mut parent,
-            ..
-        } = self;
-
-        let component = if let Some(else_body) = else_body {
-            Branch::new_with_else(condition, if_body, else_body)
-        } else {
-            Branch::new(condition, if_body)
-        };
-        parent.integrate(component);
-        parent
     }
 }
