d2bd283e9dd79d514ed372e57dfc10876edc6f77
Saethox <joni.htruw@gmail.com>
2022-06-20 13:48:02 +0200

    Add single and multi-objective fitness replacements

--- src/framework/fitness.rs
+++ src/framework/fitness.rs
@@ -78,7 +78,7 @@ pub enum IllegalFitness {
     NegativeInfinity,
 }
 
-impl std::fmt::Display for IllegalFitness {
+impl fmt::Display for IllegalFitness {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
         write!(
             fmt,
@@ -88,5 +88,186 @@ impl std::fmt::Display for IllegalFitness {
                 IllegalFitness::NegativeInfinity => "-Inf",
             },
         )
+    }
+}
+
+pub trait Objective: fmt::Debug + Clone + Eq {
+    fn is_finite(&self) -> bool;
+    fn is_valid(&self) -> bool;
+
+    fn is_single(&self) -> bool;
+    fn is_multi(&self) -> bool;
+
+    fn try_single(self) -> Result<Cost, IllegalObjective>;
+    fn try_multi(self) -> Result<Objectives, IllegalObjective>;
+
+    fn single(self) -> Cost {
+        self.try_single().unwrap()
+    }
+    fn multi(self) -> Objectives {
+        self.try_multi().unwrap()
+    }
+}
+
+/// Error type for illegal objective values.
+///
+/// Currently, `NaN` and `-Inf` are considered illegal.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum IllegalObjective {
+    NaN,
+    NegativeInfinity,
+    Unevaluated,
+    WrongType,
+}
+
+impl fmt::Display for IllegalObjective {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(
+            fmt,
+            "illegal objective: {}",
+            match self {
+                IllegalObjective::NaN => "NaN",
+                IllegalObjective::NegativeInfinity => "-Inf",
+                IllegalObjective::Unevaluated => "Unevaluated",
+                IllegalObjective::WrongType => "Wrong objective type",
+            },
+        )
+    }
+}
+
+#[derive(Debug, Clone, Copy, serde::Serialize)]
+pub struct Cost(Option<f64>);
+
+impl PartialEq for Cost {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
+}
+impl Eq for Cost {}
+
+impl PartialOrd for Cost {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.0.partial_cmp(&other.0)
+    }
+}
+impl Ord for Cost {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
+
+impl Objective for Cost {
+    fn is_finite(&self) -> bool {
+        self.is_valid() && self.0.unwrap().is_finite()
+    }
+
+    fn is_valid(&self) -> bool {
+        self.0.is_some()
+    }
+
+    fn is_single(&self) -> bool {
+        true
+    }
+
+    fn is_multi(&self) -> bool {
+        false
+    }
+
+    fn try_single(self) -> Result<Cost, IllegalObjective> {
+        Ok(self)
+    }
+
+    fn try_multi(self) -> Result<Objectives, IllegalObjective> {
+        Err(IllegalObjective::WrongType)
+    }
+}
+
+impl Cost {
+    pub fn option(self) -> Option<f64> {
+        self.0
+    }
+
+    pub fn value(self) -> f64 {
+        self.0.unwrap()
+    }
+}
+
+impl TryFrom<Cost> for f64 {
+    type Error = IllegalObjective;
+
+    fn try_from(value: Cost) -> Result<Self, Self::Error> {
+        value.option().ok_or(IllegalObjective::Unevaluated)
+    }
+}
+
+impl TryFrom<f64> for Cost {
+    type Error = IllegalObjective;
+
+    /// Tries to convert a float into a `Fitness` value.
+    ///
+    /// See [IllegalFitness] for a list of illegal values.
+    /// All other values will return `Ok`.
+    fn try_from(value: f64) -> Result<Self, IllegalObjective> {
+        match value {
+            _ if value.is_nan() => Err(IllegalObjective::NaN),
+            _ if value.is_infinite() && value.is_sign_negative() => {
+                Err(IllegalObjective::NegativeInfinity)
+            }
+            _ => Ok(Cost(Some(value))),
+        }
+    }
+}
+
+#[derive(Debug, Clone, serde::Serialize)]
+pub struct Objectives(Option<Vec<f64>>);
+
+impl PartialEq for Objectives {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
+}
+impl Eq for Objectives {}
+
+impl Objective for Objectives {
+    fn is_finite(&self) -> bool {
+        self.is_valid() && self.0.as_ref().unwrap().iter().all(|o| o.is_finite())
+    }
+
+    fn is_valid(&self) -> bool {
+        self.0.is_some()
+    }
+
+    fn is_single(&self) -> bool {
+        false
+    }
+
+    fn is_multi(&self) -> bool {
+        true
+    }
+
+    fn try_single(self) -> Result<Cost, IllegalObjective> {
+        Err(IllegalObjective::WrongType)
+    }
+
+    fn try_multi(self) -> Result<Objectives, IllegalObjective> {
+        Ok(self)
+    }
+}
+
+impl Objectives {
+    pub fn option(self) -> Option<Vec<f64>> {
+        self.0
+    }
+
+    pub fn value(self) -> Vec<f64> {
+        self.0.unwrap()
+    }
+}
+
+impl TryFrom<Objectives> for Vec<f64> {
+    type Error = IllegalObjective;
+
+    fn try_from(value: Objectives) -> Result<Self, Self::Error> {
+        value.option().ok_or(IllegalObjective::Unevaluated)
     }
 }
