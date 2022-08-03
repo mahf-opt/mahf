74b2afaac735ef2b03cc31770a691b897f5cb4f0
Saethox <joni.htruw@gmail.com>
2022-08-03 13:33:54 +0200

    Modify example

--- examples/tsp.rs
+++ examples/tsp.rs
@@ -1,10 +1,18 @@
-use mahf::heuristics::aco;
-use mahf::{framework, problems};
+use mahf::{framework, problems, heuristics::*, operators::*, tracking::Logger};
 
 fn main() -> anyhow::Result<()> {
     let problem = problems::tsp::Instances::BERLIN52.load();
-    let tau0 = 1. / problem.best_fitness().unwrap();
-    let config = aco::min_max_ant_system(20, 1., 1., tau0, 0.1, 1., 0.1, 500);
+    // let tau0 = 1. / problem.best_fitness().unwrap();
+    // let config = aco::min_max_ant_system(20, 1., 1., tau0, 0.1, 1., 0.1, 500);
+
+    let config = ils::permutation_iterated_local_search(ils::PermutationParameters {
+        local_search_params: ls::PermutationParameters {
+            n_neighbors: 20,
+            pm: 0.7,
+            n_swap: 5
+        },
+        local_search_termination: termination::FixedIterations::new(100),
+    }, termination::FixedIterations::new(10), Logger::default());
 
     let state = framework::run(&problem, &config, None);
 
