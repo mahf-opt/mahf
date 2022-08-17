0cbd1bb4a999f1c0f48287e27a0b60d67dd73352
Saethox <joni.htruw@gmail.com>
2022-08-17 16:35:38 +0200

    Add a prelude

---
+++ src/prelude.rs
@@ -0,0 +1,3 @@
+//! The MAHF prelude imports the most relevant modules, structs and traits you may need for experiments.
+
+pub use crate::{framework, heuristics::*, operators::*, problems, tracking};
