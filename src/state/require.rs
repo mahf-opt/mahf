db451861c793bf26fd37c56fa6ca3284d1b9bf83
Jonathan Wurth <jonathan.wurth@uni-a.de>
2023-08-07 16:20:48 +0200

    Enable more complex state requirements

--- src/state/require.rs
+++ src/state/require.rs
@@ -58,4 +58,9 @@ impl<'a, 'b, P> StateReq<'a, 'b, P> {
             .then_some(())
             .ok_or_else(StateError::required_missing::<Source, T>)
     }
+
+    /// Returns the `state` for advanced requirements.
+    pub fn state<'c>(&'c self) -> &'c State<'b, P> {
+        self.0
+    }
 }
